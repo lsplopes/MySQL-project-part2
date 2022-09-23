62012ab11dedb447bdc12d8d4a590d7cca49a707
LucasLopes <lopes.sp.lucas@gmail.com>
2022-09-23 18:00:11 -0300

    resolve requisito 4

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+SELECT
+	u.user AS usuario,
+  IF(MAX(YEAR(h.play_date)) >= '2021', 'Usuário ativo', 'Usuário inativo') AS status_usuario
+FROM SpotifyClone.users AS u
+INNER JOIN SpotifyClone.history AS h
+	ON u.user_id = h.user_id
+GROUP BY u.user_id
+ORDER BY u.user;
